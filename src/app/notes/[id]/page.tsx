1d937fcbe75e0409d6f6f5f2cea60014ccf606a0
Agung Ardiyanto <agungd3v@gmail.com>
2025-09-15 16:31:19 +0700

    feat: change title meta data & condition view note

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,17 +1,22 @@
 import NoteDetail from "@/components/NoteDetail";
 import NotFound from "@/components/NoutFound";
+import AuthRepository from "@/repository/auth_repository";
 import NoteRepository from "@/repository/note_repository";
 
 const noteRepo = new NoteRepository();
+const authRepo = new AuthRepository();
 
 export default async function NoteDetailPage(context: any) {
   const {id} = await context.params;
   const getNoteDetail = await noteRepo.getNoteDetail({noteId: id});
+  const getUser = await authRepo.getUser();
 
   if (!getNoteDetail) return <NotFound />
 
-  const data = getNoteDetail.toJSON();
-  if (data.visibility === "private") return <NotFound />
+  const note = getNoteDetail.toJSON();
 
-  return <NoteDetail data={data} />;
+  if (note.visibility === "hidden") return <NotFound />
+  if (note.visibility === "private" && !getUser) return <NotFound />
+
+  return <NoteDetail data={note} />;
 }
